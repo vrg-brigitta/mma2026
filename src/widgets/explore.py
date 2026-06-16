9d6265507bdb65943fdc0a6f5d4f376ad5deeecb
Kim <koomen@protonmail.com>
2026-06-16 15:57:46 +0200

    feat: add dummy elements for describe + results

--- src/widgets/explore.py
+++ src/widgets/explore.py
@@ -18,14 +18,10 @@ def create_explore_tab_widget():
                     "Submit",
                     id="submit-question-btn",
                     className="btn btn-primary mt-2",
-                    disabled=False
                 ),
 
                 html.Div(id="pipeline-status-container", className="mt-2"),
-            ],
-                direction="horizontal",
-                gap=2,
-            ),
+            ], direction="horizontal", gap=2),
 
             dcc.Interval(
                 id="ui-tick",
@@ -38,5 +34,35 @@ def create_explore_tab_widget():
                 id="pipeline-run-store",
                 data={}
             ),
+
+            html.Hr(),
+
+            dbc.Stack([
+                html.H5("Describe"),
+                html.P("Click on the button below to describe the data that is visible in the canvas."),
+                dbc.Stack([
+                    html.Button(
+                        "Submit",
+                        id="describe-btn",
+                        className="btn btn-outline-primary",
+                    ),
+                ], direction="horizontal", gap=2),
+            ], direction="vertical", className="explore-results"),
+
+            html.Hr(),
+
+            dbc.Stack([
+                html.H5("Results"),
+                html.P("Found 9318 images that may depict grief. Among these, 7882 (84.6%) are related to Asian cultures."),
+                dbc.Stack([
+                    html.Div(html.Img(src="https://picsum.photos/200/300"), className="explore-results-item"),
+                    html.Div(html.Img(src="https://picsum.photos/200/300"), className="explore-results-item"),
+                    html.Div(html.Img(src="https://picsum.photos/200/300"), className="explore-results-item"),
+                    html.Div(html.Img(src="https://picsum.photos/200/300"), className="explore-results-item"),
+                    html.Div(html.Img(src="https://picsum.photos/200/300"), className="explore-results-item"),
+                    html.Div(html.Img(src="https://picsum.photos/200/300"), className="explore-results-item"),
+                ], className="explore-results-grid")
+            ], direction="vertical", className="explore-results"),
+
         ]),
     ], className="sidebar-container border-widget")
