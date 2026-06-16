403485464bf9ee9923bc1daf5088fd990484cdc5
Kim <koomen@protonmail.com>
2026-06-16 12:38:58 +0200

    style: cleanup `src/main.py`

--- src/main.py
+++ src/main.py
@@ -9,55 +9,30 @@ from src.dataloaders.art_dataset_loader import DB_FILE_PATH
 from dash import Dash, html, dcc
 from src import config
 from src.Dataset import Dataset
-from src.widgets import projection_radio_buttons, gallery, scatterplot, wordcloud, graph, heatmap, histogram, help_popup, pipeline
-from src.widgets.table import create_table
-from src.widgets import exploration
+from src.widgets import scatterplot, pipeline
 import dash_bootstrap_components as dbc
 
 # need to import callbacks to register them, even if they are not used directly in this file
-import callbacks.table
 import callbacks.scatterplot
-import callbacks.projection_radio_buttons
-#import callbacks.heatmap
-import callbacks.wordcloud
-import callbacks.histogram
-import callbacks.gallery
-#import callbacks.graph
-import callbacks.exploration
-import callbacks.image_generator
 import callbacks.pipeline_builder
 
 def run_ui():
     external_stylesheets = [dbc.themes.BOOTSTRAP]
     app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-    help_popup_widget = help_popup.create_help_popup()
     scatterplot_widget = scatterplot.create_scatterplot(config.DEFAULT_PROJECTION)
 
     pipeline_tab_widget = pipeline.create_pipeline_tab_widget()
-    explore_tab_widget = exploration.create_explore_tab_widget()
-
-    table_widget = create_table()
 
     right_tab = dcc.Tabs([
         dcc.Tab(label='Pipeline', children=pipeline_tab_widget),
-        dcc.Tab(label='Explore', children=explore_tab_widget),
     ])
 
     app.layout = dbc.Container([
-        help_popup_widget,
-        dbc.Stack([
-            dbc.Label('Art Exploration Dashboard', className='header-title'),
-            # dbc.Button('Deselect everything', id='deselect-button', class_name="btn btn-outline-primary ms-auto header-button"),
-            # dbc.Button('Help', id='help-button', class_name="btn btn-outline-primary header-button")
-        ], id='header', direction="horizontal"),
         dbc.Row([
             dbc.Col(scatterplot_widget, width=8, className='main-col'),
-            dbc.Col(right_tab, width=4, className='main-col')],
-            justify='between', className='main-row'),
-        dbc.Row([
-            dbc.Col(table_widget, className='main-col')
-        ], className='hidden-row')
+            dbc.Col(right_tab, width=4, className='main-col')
+        ], justify='between', className='main-row'),
     ], fluid=True, id='container')
 
     app.run(debug=True, use_reloader=False)
